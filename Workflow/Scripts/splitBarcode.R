47472c512d680ff711ac1a10db9c04f671e0302c
MaartenPostuma <smart-post@hotmail.com>
2025-04-07 15:44:25 +0200

    Update splitBarcode.R

--- Workflow/Scripts/splitBarcode.R
+++ Workflow/Scripts/splitBarcode.R
@@ -4,7 +4,7 @@ args = commandArgs(trailingOnly=TRUE)
 barcodes<-read.table(args[1],h=T,sep="\t")
 
 barcodeSub<-barcodes[grep(args[3], barcodes$Raw_R1)]
-barcodeStacks<-data.frame(barcode1=paste0(barcodes$Barcode_R1   ,"C"),
-                          barcode2=paste0(barcodes$Barcode_R2,"C"),
-                          sample=barcodes$Sample)
+barcodeStacks<-data.frame(barcode1=paste0(barcodeSub$Barcode_R1   ,"C"),
+                          barcode2=paste0(barcodeSub$Barcode_R2,"C"),
+                          sample=barcodeSub$Sample)
 write.table(barcodeStacks,paste0(args[2]),row.names = F,col.names = F,quote=F,sep="\t")
